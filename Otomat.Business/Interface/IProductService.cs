e6a6c449fa6029c3b7db853df2a97e2d41612ea3
Emre Kara <mr.emrekara@outlook.com>
2021-07-08 14:23:57 +0300

    commit

--- Otomat.Business/Interface/IProductService.cs
+++ Otomat.Business/Interface/IProductService.cs
@@ -5,7 +5,16 @@ namespace Otomat.Business.Interface
 {
     public interface IProductService
     {
+        /// <summary>
+        /// Ürünleri listeler.
+        /// </summary> 
+        /// <returns> List ProductDto products </returns>
         List<ProductDto> GetList();
+        /// <summary>
+        /// İndirim oranına göre ürünleri listeler.
+        /// </summary>
+        /// <param name="discount"></param>
+        /// <returns> List ProductDto products </returns>
         List<ProductDto> GetDiscountProducts(double discount);
 
         
