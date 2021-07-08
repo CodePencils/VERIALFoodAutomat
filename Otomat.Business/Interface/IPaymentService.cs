eec1c696ba2ecd619d644f5ca08537048c263f60
Emre Kara <mr.emrekara@outlook.com>
2021-07-08 14:26:32 +0300

    commit

--- Otomat.Business/Interface/IPaymentService.cs
+++ Otomat.Business/Interface/IPaymentService.cs
@@ -5,7 +5,16 @@ namespace Otomat.Business.Interface
 {
     public interface IPaymentService
     {
+        /// <summary>
+        /// Ödeme yöntemlerini listeler
+        /// </summary> 
+        /// <returns> List PaymentDto payments </returns>
         List<PaymentDto> GetList();
+        /// <summary>
+        /// Ürün satışı yapılır geri fiş döner.
+        /// </summary>
+        /// <param name="ByProductDto"></param>
+        /// <returns> ReceiptDto receiptdto </returns>
         public ReceiptDto Buy(ByProductDto byProductDto);
     }
 }
