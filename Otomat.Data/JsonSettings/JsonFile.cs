a9b7efb5d8a3fdee12dede734e43add77147df44
Emre Kara <mr.emrekara@outlook.com>
2021-07-08 15:28:46 +0300

    CHEKİN

--- Otomat.Data/JsonSettings/JsonFile.cs
+++ Otomat.Data/JsonSettings/JsonFile.cs
@@ -9,12 +9,16 @@ namespace Otomat.Data.JsonSettings
 {
     public class JsonFile
     {
+        public JsonFile()
+        {
+
+        }
         /// <summary>
         /// <br></br>
         /// JSON Data yolu
         /// <br></br>
         /// C:\Users\Emre\source\FoodAutomat\Otomat.Data\JsonData\
         /// </summary>
-        public static string FileDirectory = @"C:\Users\Emre\source\FoodAutomat\Otomat.Data\JsonData\";
+        public static string FileDirectory = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))+ @"\VERIALFoodAutomat\Otomat.Data\JsonData\";
     }
 }
